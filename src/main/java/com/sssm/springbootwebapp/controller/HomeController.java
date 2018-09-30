fc8b9a26f25b8a38468a31be90d6402c61b275cc
Mandar.Shidhore <mandar.shidhore@travelport.com>
2018-09-29 19:48:25 -0600

    changes related to moving jsp

--- src/main/java/com/sssm/springbootwebapp/controller/HomeController.java
+++ src/main/java/com/sssm/springbootwebapp/controller/HomeController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
-	
+
 	// this method gets rid of localhost:8080 whitelabel error (encountered in absence of default mapping)
 	// it redirects to @RequestMapping("/home") method
 	@RequestMapping("/")
@@ -19,7 +19,12 @@ public class HomeController {
 	@RequestMapping("/home")
 	public String helloWorld() {
 		System.out.println(">> helloWorld");
-		// path to jsp - if jsp is directly under webapp, then no need to add /WEB-INF/
-		return "/WEB-INF/home.jsp";
+		// if application.properties file does not contain prefix and suffix for jsp pages
+		// 		1. use return "/WEB-INF/home.jsp";
+		//		2. if jsp is directly under webapp, then no need to add /WEB-INF/
+		// if application.properties file contains prefix and suffix for jsp pages
+		//		1. just use return "home";
+		return "home";
 	}
+
 }
--- src/main/webapp/WEB-INF/jsp/home.jsp
+++ src/main/webapp/WEB-INF/jsp/home.jsp
@@ -3,6 +3,6 @@
 	<title>Spring Boot Web App Basic</title>
 </head>
 <body>
-	Hello World jsp
+	Hello World from home.jsp in folder src/main/webapp/WEB-INF/jsp
 </body>
 </html>
