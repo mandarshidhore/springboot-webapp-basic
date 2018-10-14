b580c35f1390a5e5e5762bcc300696a46ea93e6e
Mandar Shidhore <mandar.shidhore@yahoo.com>
2018-10-13 19:05:12 -0600

    comments + devtools + line breaks in jsp

--- src/main/java/com/sssm/springbootwebapp/controller/HomeController.java
+++ src/main/java/com/sssm/springbootwebapp/controller/HomeController.java
@@ -6,9 +6,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HomeController {
 
-	// this method gets rid of localhost:8080 whitelabel error (encountered in
-	// absence of default mapping) by redirecting to @RequestMapping("/home") method
 	// url - http://localhost:8080
+	// this gets rid of localhost:8080 whitelabel error (that is in
+	// absence of default mapping) by redirecting to @RequestMapping("/home") method
+	// note - if @Controller is changed to @RestController, redirect:/home will be
+	// printed on screen
 	@RequestMapping("/")
 	public String welcome() {
 		System.out.println(">> welcome");
--- src/main/webapp/WEB-INF/jsp/input.jsp
+++ src/main/webapp/WEB-INF/jsp/input.jsp
@@ -4,8 +4,9 @@
 </head>
 <body>
 	first name = ${f_name}
+	<br/>
 	last name = ${l_name}
-	
+	<br/>
 	Three User Inputs ->
 		${tui.fi}
 		${tui.si}
